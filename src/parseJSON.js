3ca0d77ff870bbeb0d530eeff35d8440a264417c
Justin Chow <hrll71990@gmail.com>
2020-11-27 15:17:51 -0800

    started pseudocoding grammar handling for parseJSON and tests

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,11 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  var result;
+  console.log(json);
+  
+  // need function that advances one character in json string at a time
+
+  // need functions that handle each piece of json grammar?
+  // obj, member, pair, array, elements, values: string, number, bool, null
 };
